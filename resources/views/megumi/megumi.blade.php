6c642b1b97f176b9301714deb4a238ba23cf15c7
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-06-29 08:16:41 +0000

    ojamajo

--- resources/views/megumi/megumi.blade.php
+++ resources/views/megumi/megumi.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-   <ul class= list>
-       <li><a href='#'>megumi</a></li>
-       <li><a href='#'>>no</a></li>
-       <li><a href='#'>egao</a></li>
-    </ul>
+<?php 
+$uranai[1]  = "みずがめ座：フナミ"; 
+$uranai[2]  = "うお座：みなみ"; 
+$uranai[3]  = "おひつじ座：めぐちゃん"; 
+$uranai[4]  = "おうし座"; 
+$uranai[5]  = "ふたご座：ざきお"; 
+$uranai[6]  = "かに座"; 
+$uranai[7]  = "しし座"; 
+$uranai[8]  = "おとめ座"; 
+$uranai[9]  = "てんびん座：じっちゃん"; 
+$uranai[10] = "さそり座";
+$uranai[11] = "いて座";
+$uranai[12] = "やぎ座：ゆのき";
+
+$figs = range(1, 12);
+$nums = range(1, 12); 
+shuffle($nums);
+
+
+?>
+
+@foreach (array_map(null, $nums, $figs) as [$num, $fig])
+    <h1>
+     {{$fig}} 位 {{$uranai[$num]}}
+    </h1>
+@endforeach
+
+
 @endsection
